111ceb6d00f4ca046eb2dc2acd27c3018deb8244
J. Lowell Wofford <lowell@lanl.gov>
2021-03-24 11:11:45 -0600

    add kraken-layercake(-vbox) generated commands

---
+++ cmd/kraken-layercake-vbox/includes.go
@@ -0,0 +1,34 @@
+/* includes.go.tpl: used to include modules & extensions specified in build config
+ *
+ * Author: J. Lowell Wofford <lowell@lanl.gov>
+ *
+ * This software is open source software available under the BSD-3 license.
+ * Copyright (c) 2018, Triad National Security, LLC
+ * See LICENSE file for details.
+ */
+
+package main
+
+// Extensions
+
+import (
+	_ "github.com/kraken-hpc/kraken-layercake/extensions/imageapi"
+
+	_ "github.com/kraken-hpc/kraken/extensions/ipv4"
+
+	_ "github.com/kraken-hpc/kraken-layercake/extensions/pxe"
+
+	_ "github.com/kraken-hpc/kraken-layercake/extensions/vbox"
+
+	// Modules
+
+	_ "github.com/kraken-hpc/kraken-layercake/modules/imageapi"
+
+	_ "github.com/kraken-hpc/kraken-layercake/modules/pxe"
+
+	_ "github.com/kraken-hpc/kraken/modules/restapi"
+
+	_ "github.com/kraken-hpc/kraken-layercake/modules/vboxmanage"
+
+	_ "github.com/kraken-hpc/kraken/modules/websocket"
+)
