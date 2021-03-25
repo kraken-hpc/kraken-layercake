2b89644b724b52803b7ee131f78a4ca3abb16e3a
J. Lowell Wofford <lowell@lanl.gov>
2021-03-25 13:38:04 -0600

    Better layer0 building scripts (#10)
    * move to always using busybox u-root build; rewrite & rename buildlayer0 -> build-layer0-base.sh
    
    * build-layer0-base - update description text, tmpdir format
    
    * add build-layer0-kmod.sh for building kernel module overlay bundles
    
    * add -h option for both build-layer0 scripts
    
    * update layer0 readme

---
+++ utils/layer0/build-layer0-base.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+###
+# This will build a layer0 image.
+#
+# Notes:
+# - We build in a dedicated "tmp_dir", including a separate GOPATH.  This helps ensure consistent builds.
+# - You can keep (-k) and reuse a tmp_dir (-t <tmp_dir>), which can speed up builds significantly when frequently rebuilding/testing.
+# - You can overlay a base directory by providing a second argument.  However, we recommend concatinated CPIOs instead.
+#
+###
+
+usage() {
+        echo "Usage: $0 [-kh] [-o <out_file>] [-b <base_dir>] [ -t <tmp_dir> ] <arch> [<additional_go_cmd> ...]"
+        echo "  <arch> should be the GOARCH we want to build (e.g. arm64, amd64...)"
+        echo "  <out_file> is the file the image should be written to.  (default: layer0-00-base.<date>.<arch>.cpio.xz)"
+        echo "  <base_dir> is an optional base directory containing file/directory structure (default: none)"
+        echo "             that should be added to the image"
+        echo "  <tmp_dir> is a temporary directory to use.  This can be used to resume a previous build"
+        echo "            IMPORTANT: tmp_dir cannot sit inside of a moduled go directory!"
+        echo "  <additional_go_cmd> is a go cmd path spec that should be built into the busybox"
+        echo "  [-k] keep temporary directory (do not delete)"
+        echo "  [-h] display this usage information and exit"
+}
+
+# Exit with a failure message
+fatal() {
+    echo "$1" >&2
+    exit 1
+}
+
+if ! opts=$(getopt o:b:t:kh "$@"); then
+    usage
+    exit
+fi
+
+DELETE_TMPDIR=1
+# shellcheck disable=SC2086
+set -- $opts
+for i; do
+    case "$i"
+    in
+        -h)
+            usage
+            exit
+            shift; shift;;
+        -o)
+            echo "Output file is $2"
+            OUTFILE="$2"
+            shift; shift;;
+        -b)
+            echo "Using base dir $2"
+            BASEDIR="$2"
+            shift; shift;;
+        -t) echo "Using tmp dir $2"
+            TMPDIR=$(readlink -f "$2")
+            shift; shift;;
+        -k) echo "Will not delete temporary directory at the end"
+            DELETE_TMPDIR=0
+            shift;;
+        --)
+            shift; break;;
+    esac
+done
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+ARCH=$1
+shift
+
+# Commands to build into u-root busybox
+EXTRA_COMMANDS=()
+EXTRA_COMMANDS+=( github.com/kraken-hpc/kraken-layercake/cmd/kraken-layercake )
+EXTRA_COMMANDS+=( github.com/kraken-hpc/uinit/cmds/uinit )
+EXTRA_COMMANDS+=( github.com/kraken-hpc/imageapi/cmd/imageapi-server )
+EXTRA_COMMANDS+=( github.com/jlowellwofford/entropy/cmd/entropy )
+EXTRA_COMMANDS+=( github.com/bensallen/modscan/cmd/modscan )
+EXTRA_COMMANDS+=( github.com/bensallen/rbd/cmd/rbd )
+
+while (( "$#" )); do 
+    echo "Adding command $1"
+    EXTRA_COMMANDS+=( "$1" )
+    shift
+done
+
+UROOT="github.com/u-root/u-root"
+
+# make a temporary directory for our base
+if [ -z ${TMPDIR+x} ]; then
+    TMPDIR="$PWD/$(mktemp -tmpdir -d layer0-base.XXXXXXXXXXXX)"
+else 
+    if [ ! -d "$TMPDIR" ]; then
+        echo "Creating $TMPDIR"
+        mkdir -p "$TMPDIR" || fatal "failed to create $TMPDIR"
+    fi
+fi
+echo "Using tmpdir: $TMPDIR"
+ORIG_PWD="$PWD"
+cd "$TMPDIR" || fatal "couldn't cd to $TMPDIR"
+GOPATH="$TMPDIR/gopath"
+
+EXTRA_MODS=()
+# Make sure commands are available
+for c in "${EXTRA_COMMANDS[@]}"; do
+   if [ ! -d "$GOPATH/src/$c" ]; then
+    echo "installing $c"
+    GOPATH=$GOPATH GO111MODULE=off go get "$c" || fatal "failed to install $c"
+   fi
+    cd "$GOPATH/src/$c" || fatal "couldn't cd to $GOPATH/src/$c"
+    go mod edit -replace=github.com/u-root/u-root="$GOPATH/src/$UROOT"
+    MOD=$(go mod edit -print | awk '$1=="module"{print $2}')
+    echo "mod: $MOD"
+    EXTRA_MODS+=( "$MOD" )
+done
+
+# fixup mod deps
+for c in "${EXTRA_COMMANDS[@]}"; do
+    (
+        cd "$GOPATH/src/$c" || fatal "couldn't cd to $GOPATH/src/$c"
+        if [ -d "vendor" ]; then
+            echo "Removing vendor folder $PWD/vendor"
+            rm -rf "$PWD/vendor"
+        fi
+        for m in "${EXTRA_MODS[@]}"; do 
+            go mod edit -replace="$m=$GOPATH/src/$m"
+        done
+    )
+done
+
+# Check that gobusybox is installed, clone it if not
+if [ ! -x "$GOPATH"/bin/makebb ]; then
+    echo "installing gobusybox"
+    GOPATH="$GOPATH" GO111MODULE=off go get github.com/u-root/gobusybox/src/cmd/makebb || fatal "failed to install gobusybox"
+fi
+
+# Check that u-root is installed, clone it if not
+if [ ! -x "$GOPATH"/bin/u-root ]; then
+    echo "installing u-root"
+    GOPATH="$GOPATH" GO111MODULE=off go get "$UROOT" || fatal "failed to install u-root"
+fi
+
+# Generate the array of commands to add to BusyBox binary
+BB_COMMANDS=( "$GOPATH/src/$UROOT"/cmds/{core,boot,exp}/* )
+# shellcheck disable=SC2068
+for cmd in ${EXTRA_COMMANDS[@]}; do
+    BB_COMMANDS+=( "$GOPATH"/src/"$cmd" )
+done
+
+# Create BusyBox binary (outside of u-root)
+echo "Creating BusyBox binary..."
+mkdir -p "$TMPDIR"/base/bbin
+printf "Command list: %s" "${BB_COMMANDS[@]}"
+# shellcheck disable=SC2068
+"$GOPATH"/bin/makebb -o "$TMPDIR"/base/bbin/bb ${BB_COMMANDS[@]} || fatal "makebb: failed to create BusyBox binary"
+
+# Create symlinks of included programs to BusyBox binary
+echo "Creating links to BusyBox binary..."
+# shellcheck disable=SC2068
+for cmd in ${BB_COMMANDS[@]}; do
+    ln -s bb "$TMPDIR"/base/bbin/"$(basename "$TMPDIR"/base/bbin/"$cmd")"
+done
+
+# copy base_dir over tmpdir if it's set
+if [ -n "${BASEDIR+x}" ]; then
+        echo "Overlaying ${BASEDIR}..."
+        rsync -av "$BASEDIR"/ "$TMPDIR"/base
+fi
+
+echo "Creating base cpio..."
+(
+    cd "$TMPDIR"/base || exit 1
+    find . | cpio -oc > "$TMPDIR"/base.cpio
+) || fatal "Creating base cpio failed"
+
+echo "Creating image..."
+# shellcheck disable=SC2068
+GOARCH="$ARCH" "$GOPATH"/bin/u-root -nocmd -initcmd=/bbin/init -uinitcmd=/bbin/uinit -defaultsh=/bbin/elvish -base "$TMPDIR"/base.cpio -o "$TMPDIR"/initramfs.cpio 2>&1
+
+echo "CONTENTS:"
+cpio -itv < "$TMPDIR"/initramfs.cpio
+
+echo "Compressing..."
+xz "$TMPDIR"/initramfs.cpio
+
+if [ -z "${OUTFILE+x}" ]; then
+    D=$(date +%Y%m%d.%H%M)
+    OUTFILE="layer0-00-base.${D}.${ARCH}.cpio.xz"
+fi
+mv -v "$TMPDIR"/initramfs.cpio.xz "$ORIG_PWD"/"$OUTFILE"
+
+if [ $DELETE_TMPDIR -eq 1 ]; then
+    echo "Removing temporary directory"
+    rm -rf "$TMPDIR"
+fi
+
+echo "Image built as $OUTFILE"
---
+++ utils/layer0/build-layer0-kmod.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+###
+# This will build a layer0 kmod bundle that can be layered on the base bundle
+###
+
+usage() {
+   echo "Usage: $0 [-kha] [-o <out_file>] [-c <chroot_dir>] [-t <tmp_dir>] [-f <mod_file>] <kernel_version> [<mod> ...]"
+   echo "  <out_file> file the image should be written to. (default: layer0-10-kmod.<kernel_version>.cpio.xz)"
+   echo "  <chroot_dir> chroot to look for kernel modules in (currently not implemented)"
+   echo "  <tmp_dir> is a temporary directory to use.  This can be used to resume a previous build"
+   echo "  <mod_file> a file to read a list of modules from.  Can contain blank lines and comments beginning with #."
+   echo "  <kernel_version> the kernel version string to build the kmod bundle for.  For current kernel, use $(uname -r)."
+   echo "  <mod> ... modules to be added to the kmod bundle (can be combined with <mod_file>)"
+   echo "  [-k] keep termporary directory (do not delete)"
+   echo "  [-a] install all available modules (ignores <mod_file> and <mod>)"
+   echo "  [-h] display this usage information and exit"
+}
+
+fatal() {
+   echo "$1" >&2
+   exit 1
+}
+
+build_modlist_all() {
+   echo "Building modlist (all)"
+   for m in $( find "/lib/modules/$KVER" -name '*.ko*' ); do
+      MODLIST+=( $m )
+   done
+}
+
+build_modlist() {
+   echo "Building modlist"
+   if [ -n "${MODFILE+x}" ]; then
+      if [ ! -f "$MODFILE" ]; then
+         fatal "Modfile does not exist $MODFILE"
+      fi
+      echo "Reading module list from $MODFILE"
+      for m in $( cat "$MODFILE" | grep -vE '^#|^$' || fatal "couldn't read $MODFILE" ); do
+         NAMES+=( "$m" )
+      done 
+   fi
+   for n in "${NAMES[@]}"; do
+      for m in $( modprobe --set-version="$KVER" --show-depends "$n" | awk '$1=="insmod"{print $2}' || fatal 'could not find module by name '$n ); do 
+         MODLIST+=( "$m" )
+      done
+   done
+
+   IFS=$'\n' MODLIST=($( sort -u <<<"${MODLIST[*]}")); unset IFS
+}
+
+if ! opts=$(getopt c:f:o:t:kah "$@"); then
+   usage
+   exit
+fi
+
+ORIG_PWD=$PWD
+DELETE_TMPDIR=1
+ALL_MODS=0
+ROOT="/"
+
+# shellcheck disable=SC2086
+set -- $opts
+for i; do
+   case "$i"
+   in
+      -h)
+         usage
+         exit
+         shift; shift;;
+      -c)
+         fatal "Chroot is not yet implemented."
+         if [ ! -d "$2" ]; then
+            fatal "Specified chroot does not exist: $2"
+         fi
+         echo "Using chroot at $2"
+         ROOT="$2"
+         shift; shift;;
+      -o) 
+         echo "Output file is $2" 
+         OUTFILE="$2" 
+         shift; shift;;
+      -b) 
+         MODFILE="$2"
+         shift; shfit;;
+      -t)
+         TMPDIR=$( readlink -f "$2" || fatal "no such file $2" )
+         shift; shift;;
+      -k)
+         echo "Will not delete temporary directory at the end"
+         DELETE_TMPDIR=0
+         shift;;
+      -f)
+         MODFILE="$2"
+         shift; shift;;
+      -a)
+         echo "Building bundle with all kmods (other specifiers will be ignored)"
+         ALL_MODS=1
+         shift;;
+      --)
+         shift; break;;
+   esac
+done
+
+NAMES=()
+MODLIST=()
+
+if [ $# -lt 1 ]; then
+   usage
+   exit 1
+fi
+
+KVER="$1"
+shift
+echo "Using kernel version: $KVER"
+MODDIR=$( readlink -f "$ROOT/lib/modules/$KVER" )
+
+if [ ! -d "$MODDIR" ]; then
+   fatal "Module directory does not exist: $KVER"
+fi
+
+while (( "$#" )); do
+   NAMES+=( "$1" )
+   shift
+done
+
+if [ $ALL_MODS -eq 1 ]; then
+   build_modlist_all
+else
+   build_modlist
+fi
+
+printf "Modlist: %s\n" "${MODLIST[@]}"
+
+if [ -z ${TMPDIR+x} ]; then
+   TMPDIR=$(mktemp -tmpdir -d layer0-kmod.XXXXXXXXXXXX)
+else
+   if [ ! -d "$TMPDIR" ]; then
+      echo "Creating $TMPDIR"
+      mkdir -p "$TMPDIR" || fatal "failed to create $TMPDIR"
+   fi
+fi
+
+cd "$TMPDIR" || fatal "could not cd to $TMPDIR"
+mkdir -p "root/lib/modules/$KVER" || fatal "could not create module directory"
+
+echo "Installing modules"
+for m in "${MODLIST[@]}"; do
+   b=$( basename $m )
+   d=$( dirname $m )
+   ddir="$TMPDIR/root$d"
+   mkdir -p "$ddir" || fatal "could not mkdir $ddir"
+   /bin/cp -Lv "$m" "$ddir/$b" || fatal "could not copy module $m"
+done
+
+echo "Copying module metadata files"
+cp -Lv "/lib/modules/$KVER/modules.order" "$TMPDIR/root/lib/modules/$KVER/"
+cp -Lv "/lib/modules/$KVER/modules.builtin" "$TMPDIR/root/lib/modules/$KVER/"
+
+echo "Running depmod"
+depmod -w --basedir "$TMPDIR/root" --all --verbose "$KVER"
+
+if [ -z ${OUTFILE+x} ]; then
+   OUTFILE="layer0-10-kmod.$KVER.cpio.xz"
+fi
+
+echo "Creating compressed cpio at $OUTFILE"
+cd "$TMPDIR/root" || fatal "could not cd to $TMPDIR/root"
+find . | cpio -oc | xz -c > "$ORIG_PWD"/"$OUTFILE" || fatal "failed to compressec cpio bundle"
+
+if [ $DELETE_TMPDIR -eq 1 ]; then
+   echo "Cleaning up $TMPDIR"
+fi
